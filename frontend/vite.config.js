c77de2ec3c536e8fccf5d3106f8ad54c1f3da65c
skbhagat0502 <sandeepbh0502@gmail.com>
2023-12-22 15:40:11 +0530

    meta data added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://surebuy-api.vercel.app/",
         changeOrigin: true,
         ws: true,
       },
